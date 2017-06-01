92987e145a8b0d66d548c0f3184d2a031b190ce6
ale-cia <ahodges230@gmail.com>
2017-06-01 13:38:22 -0500

    Intro to JS

---
+++ Section 8 - Intro to JS/age.js
@@ -0,0 +1,4 @@
+//var message = prompt("Please enter your");
+var age = prompt("How old are you?");
+var daysOnPlanet = age * 365.25;
+alert(age + "  years is roughly " + daysOnPlanet + "days " );
---
+++ Section 8 - Intro to JS/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var fullName = firstName + " " + lastName;
+var age = prompt("How old are you?");
+
+console.log("Howdy, " + fullName);
+console.log("You are " + age + " years old!");
+
