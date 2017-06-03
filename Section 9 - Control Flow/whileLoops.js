03a54eb0c3614b98baf1d1580379a638b6304cab
ale-cia <ahodges230@gmail.com>
2017-06-02 19:17:11 -0500

    While Loops

---
+++ Section 9 - Control Flow/whileLoops.js
@@ -0,0 +1,27 @@
+var count = 1;
+while(count < 10) {
+	console.log("count is: " + count);
+	count++;
+}
+
+
+
+
+var count = 15;
+while(count < 30) {
+	console.log("count is: " + count)
+	count += 3;
+}
+
+//Print each character in a string
+//string to that will be looped over
+var str = "Malawi";
+
+//first character is at index 0
+var count = 0;
+
+while (count < str.length) {
+	console.log(str[count]);
+	count++;
+}
+
---
+++ Section 9 - Control Flow/whileLoopsPractice.js
@@ -0,0 +1,27 @@
+//Explain this code//
+var num = 1;
+while(num <= 10) {
+	console.log(num);
+	num += 2;
+}
+
+//Odd numbers will be printed from 1 - 9
+//11 is displayed 
+
+var num = 1 
+while(num <= 20) {
+	if(num % 4 === 0){
+	console.log(num)
+}
+num++
+}
+
+//Numbers evenly divided by 4 will be printed
+
+var num = 100;
+while(num < 150) {
+	console.log(num + 1)
+	num--;
+}
+
+//Infinite loop
